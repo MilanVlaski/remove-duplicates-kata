17ea9fe57c47aceb42ad4431343d878bb263685d
MilanVlaski <milan.vlaski321@gmail.com>
2023-09-16 20:09:30 +0200

    added hashmap implementation

--- src/main/UniqueArray.java
+++ src/main/UniqueArray.java
@@ -1,26 +1,34 @@
 package main;
 
-import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 public class UniqueArray {
-	private final Set<Integer> set = new LinkedHashSet<>();
+	private final Map<Integer, Integer> map = new HashMap<>();
+	private int size = 0;
 	
 	public void add(int element) {
-		set.add(element);
+		if(!map.containsValue(element))
+			map.put(size++, element);
 	}
 	
 	public int size() {
-		return set.size();
+		return map.size();
 	}
 	
 	public int[] get() {
 		
-		int[] result = new int[set.size()];
-		int i = 0;
-		for (int element : set) {
-			result[i++] = element;
+		int[] result = new int[map.size()];
+		for (int i = 0; i < map.size(); i++) {			
+			result[i] = map.get(i);
 		}
 		return result;
+		
+//		int[] result = new int[set.size()];
+//		int i = 0;
+//		for (int element : set) {
+//			result[i++] = element;
+//		}
+//		return result;
 	}
 }
