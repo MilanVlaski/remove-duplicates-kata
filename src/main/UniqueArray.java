336d3db6ed627e7e93949a2b667d003a70fd9010
MilanVlaski <milan.vlaski321@gmail.com>
2023-09-16 20:49:50 +0200

    size() returns size, not map.size()

--- src/main/UniqueArray.java
+++ src/main/UniqueArray.java
@@ -6,29 +6,22 @@ import java.util.Map;
 public class UniqueArray {
 	private final Map<Integer, Integer> map = new HashMap<>();
 	private int size = 0;
-	
+
 	public void add(int element) {
-		if(!map.containsValue(element))
+		if (!map.containsValue(element))
 			map.put(size++, element);
 	}
-	
+
 	public int size() {
-		return map.size();
+		return size;
 	}
-	
+
 	public int[] get() {
-		
+
 		int[] result = new int[map.size()];
-		for (int i = 0; i < map.size(); i++) {			
+		for (int i = 0; i < map.size(); i++) 
 			result[i] = map.get(i);
-		}
-		return result;
 		
-//		int[] result = new int[set.size()];
-//		int i = 0;
-//		for (int element : set) {
-//			result[i++] = element;
-//		}
-//		return result;
+		return result;
 	}
 }
