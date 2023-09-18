36e27ed19a4849f5ec8aee7f6834c5f214e4807c
MilanVlaski <milan.vlaski321@gmail.com>
2023-09-18 11:36:32 +0200

    added linkedhashset implementation

---
+++ src/main/LinkedSetImplementation.java
@@ -0,0 +1,26 @@
+package main;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class LinkedSetImplementation {
+	private final Set<Integer> set = new LinkedHashSet<>();
+	
+	public void add(int element) {
+		set.add(element);
+	}
+	
+	public int size() {
+		return set.size();
+	}
+	
+	public int[] get() {
+		
+		int[] result = new int[set.size()];
+		int i = 0;
+		for (int element : set) {
+			result[i++] = element;
+		}
+		return result;
+	}
+}
--- src/main/UniqueArray.java
+++ src/main/UniqueArray.java
@@ -24,4 +24,5 @@ public class UniqueArray {
 
 		return result;
 	}
+	
 }
