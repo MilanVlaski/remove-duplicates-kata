be382285b678a50d7ead643424322356a04913e8
MilanVlaski <milan.vlaski321@gmail.com>
2023-09-18 11:47:01 +0200

    refactored calls to the two implementations

--- src/main/DuplicateRemover.java
+++ src/main/DuplicateRemover.java
@@ -4,20 +4,18 @@ public class DuplicateRemover {
 
 	
 	public static int[] work1(int[] input) {
-		UniqueArray uniqueArray = new LinkedSetImplementation();
-		
+		return work(input, new LinkedSetImplementation());
+	}
+	
+	public static int[] work2(int[] input) {		
+		return work(input, new HashMapImplementation());
+	}
+
+	private static int[] work(int[] input, UniqueArray uniqueArray) {
 		for (int i = 0; i < input.length; i++)
 			uniqueArray.add(input[i]);
 
 		return uniqueArray.get();
 	}
 	
-	public static int[] work2(int[] input) {		
-		UniqueArray uniqueArray = new HashMapImplementation();
-		
-		for (int i = 0; i < input.length; i++)
-			uniqueArray.add(input[i]);
-
-		return uniqueArray.get();
-	}
 }
